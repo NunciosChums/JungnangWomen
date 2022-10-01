0f625e9a7e8517899c7231d4bde6c092df466fbd
진창훈 <susemi99@gmail.com>
2022-10-01 23:15:32 +0900

    불필요한 코드 제거

--- JungnangWomen/app/src/main/java/kr/susemi99/jungnangwomen/util/DebugLogTree.kt
+++ JungnangWomen/app/src/main/java/kr/susemi99/jungnangwomen/util/DebugLogTree.kt
@@ -3,5 +3,5 @@ package kr.susemi99.jungnangwomen.util
 import timber.log.Timber
 
 class DebugLogTree : Timber.DebugTree() {
-  override fun createStackElementTag(element: StackTraceElement) = "App: (${element.fileName}:${element.lineNumber})"
+  override fun createStackElementTag(element: StackTraceElement) = "(${element.fileName}:${element.lineNumber})"
 }

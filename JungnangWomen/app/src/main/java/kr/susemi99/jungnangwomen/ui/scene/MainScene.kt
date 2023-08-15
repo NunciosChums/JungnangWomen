fe2e67cfd3eacfded478c6870aac91178a33dcce
USA_USB <nuncios05.chums@icloud.com>
2023-08-15 12:07:35 +0900

    support Android 14

--- JungnangWomen/app/src/main/java/kr/susemi99/jungnangwomen/ui/scene/MainScene.kt
+++ JungnangWomen/app/src/main/java/kr/susemi99/jungnangwomen/ui/scene/MainScene.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
@@ -19,6 +20,7 @@ import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -41,6 +43,7 @@ fun MainScene() {
     topBar = {
       TopAppBar(
         title = { Text(text = stringResource(id = R.string.app_name)) },
+        modifier = Modifier.shadow(4.dp),
         actions = {
           IconButton(onClick = {
             listItems.refresh()

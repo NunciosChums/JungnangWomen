7a11fc2cebb723222d2c24735ef1c2534e9fa9e4
USA_USB <nuncios05.chums@icloud.com>
2023-11-11 17:08:16 +0900

    라이브러이 업데이트

--- JungnangWomen/app/build.gradle.kts
+++ JungnangWomen/app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
     compose = true
   }
   composeOptions {
-    kotlinCompilerExtensionVersion = "1.5.3" // https://developer.android.com/jetpack/androidx/releases/compose-compiler
+    kotlinCompilerExtensionVersion = "1.5.4" // https://developer.android.com/jetpack/androidx/releases/compose-compiler
   }
   packaging {
     resources {
@@ -50,9 +50,9 @@ android {
 }
 
 dependencies {
-  val lifecycleVersion = "2.6.1"
+  val lifecycleVersion = "2.6.2"
 
-  val composeBom = platform("androidx.compose:compose-bom:2023.08.00") // https://developer.android.com/jetpack/compose/setup#bom-version-mapping
+  val composeBom = platform("androidx.compose:compose-bom:2023.10.01") // https://developer.android.com/jetpack/compose/setup#bom-version-mapping
   implementation(composeBom)
   androidTestImplementation(composeBom)
   debugImplementation(composeBom)
@@ -69,9 +69,9 @@ dependencies {
   androidTestImplementation("androidx.test.ext:junit:1.1.5")
   androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-  implementation("androidx.core:core-ktx:1.10.1")
+  implementation("androidx.core:core-ktx:1.12.0")
   implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
-  implementation("androidx.activity:activity-compose:1.7.2")
+  implementation("androidx.activity:activity-compose:1.8.0")
 
   // api
   implementation("com.squareup.retrofit2:retrofit:2.9.0")
@@ -88,9 +88,9 @@ dependencies {
   implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycleVersion")
 
   // pagination
-  implementation("androidx.paging:paging-compose:3.2.0")
+  implementation("androidx.paging:paging-compose:3.2.1")
 
   // hilt
-  implementation("com.google.dagger:hilt-android:2.48")
-  kapt("com.google.dagger:hilt-android-compiler:2.48")
+  implementation("com.google.dagger:hilt-android:2.48.1")
+  kapt("com.google.dagger:hilt-android-compiler:2.48.1")
 }

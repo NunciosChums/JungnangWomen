9f5b0c82ebc8dc95c5871ba2e7aa00f86e4a4ff5
NunciosChums <nuncios05.chums@icloud.com>
2023-12-21 21:24:37 +0900

    라이브러리 업데이트

--- JungnangWomen/app/build.gradle.kts
+++ JungnangWomen/app/build.gradle.kts
@@ -71,12 +71,12 @@ dependencies {
 
   implementation("androidx.core:core-ktx:1.12.0")
   implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
-  implementation("androidx.activity:activity-compose:1.8.1")
+  implementation("androidx.activity:activity-compose:1.8.2")
 
   // api
   implementation("com.squareup.retrofit2:retrofit:2.9.0")
   implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
-  implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.11")
+  implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.12")
 
   // json
   implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
@@ -91,6 +91,6 @@ dependencies {
   implementation("androidx.paging:paging-compose:3.2.1")
 
   // hilt
-  implementation("com.google.dagger:hilt-android:2.49")
-  kapt("com.google.dagger:hilt-android-compiler:2.49")
+  implementation("com.google.dagger:hilt-android:2.50")
+  kapt("com.google.dagger:hilt-android-compiler:2.50")
 }
